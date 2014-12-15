41600acbf4e42e49848034b132ddfef0fde669db
Konrad Grzanek <kongra@gmail.com>
2014-12-15 11:27:09 +0100

    Modules for data-type declarations and data-flow parts of the algorithm added.

---
+++ AI/Rete/Data.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE CPP #-}
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 702
+{-# LANGUAGE Trustworthy #-}
+#endif
+{-# OPTIONS_GHC -W -Wall #-}
+------------------------------------------------------------------------
+-- |
+-- Module      : AI.Rete.Data
+-- Copyright   : (c) 2014 Konrad Grzanek
+-- License     : BSD-style (see the file LICENSE)
+-- Created     : 2014-12-15
+-- Maintainer  : kongra@gmail.com
+-- Stability   : experimental
+------------------------------------------------------------------------
+module AI.Rete.Data where
