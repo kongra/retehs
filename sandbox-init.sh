3d7251d7fa66c859b029339001434ea663bfd2cc
Konrad Grzanek <kongra@gmail.com>
2025-01-16 16:30:06 +0100

    Update

--- sandbox-init.sh
+++ sandbox-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cabal sandbox init
-cabal sandbox add-source ../kask-base
-cabal sandbox add-source ../treeprint
+cabal sandbox add-source ../01-kask-base
+cabal sandbox add-source ../02-treeprint
